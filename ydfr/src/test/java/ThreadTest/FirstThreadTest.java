1c9aed09622c30e4224bbf3d5782553e74b65a8d
zdf <you@example.com>
2017-12-27 17:41:29 +0800

    aa

---
+++ ydfr/src/test/java/ThreadTest/FirstThreadTest.java
@@ -0,0 +1,19 @@
+package ThreadTest;
+
+
+public class FirstThreadTest {
+	public static void main(String[] args) throws InterruptedException {
+		Runnable runnable=new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("线程启动");
+				//耗时操作
+				System.out.println("线程结束");
+			}
+		};
+		Thread thread=new  Thread(runnable);//创建线程，runnable作为线程要执行的任务（载体）
+		thread.start();//启动线程
+        thread.join();//等待线程执行完毕(确定一个线程完毕才能执行下一个线程)
+	}
+
+}
---
+++ ydfr/src/test/java/ThreadTest/FourThreadTest.java
@@ -0,0 +1,81 @@
+package ThreadTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FourThreadTest {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		System.out.println("使用线程池运行 Callable 任务：");
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(5); // 创建大小固定为
+																		// 5
+																		// 的线程池
+
+		List<Future<Integer>> futures = new ArrayList<>(10);
+
+		for (int i = 0; i < 10; i++) {
+			AccumCallable task = new AccumCallable(i * 10 + 1, (i + 1) * 10);
+			Future<Integer> future = threadPool.submit(task); // 提交任务
+			futures.add(future);
+		}
+		threadPool.shutdown(); // 向线程池发送关闭的指令，等到已经提交的任务都执行完毕之后，线程池会关闭
+
+		int total = 0;
+		for (Future<Integer> future : futures) {
+			total += future.get(); // 阻塞，直到任务结束，返回任务的结果
+		}
+
+		System.out.println("Total: " + total);
+	}
+	
+	//***********用invokeAll 来执行一组的任务*************
+//	public static void main(String[] args) throws Exception {
+//	    System.out.println("使用线程池 invokeAll 运行一组 Callable 任务：");
+//
+//	    ExecutorService threadPool = Executors.newFixedThreadPool(5); // 创建大小固定为 5 的线程池
+//
+//	    List<AccumCallable> tasks = new ArrayList<>(10); // tasks 为一组任务
+//	    for (int i = 0; i < 10; i++) {
+//	        tasks.add(new AccumCallable(i * 10 + 1, (i + 1) * 10)); 
+//	    }
+//
+//	    List<Future<Integer>> futures = threadPool.invokeAll(tasks); // 阻塞，直到所有任务都运行完毕
+//
+//	    int total = 0;
+//	    for (Future<Integer> future : futures) {
+//	        total += future.get(); // 返回任务的结果
+//	    }
+//
+//	    System.out.println("Total: " + total);
+//
+//	    threadPool.shutdown(); // 向线程池发送关闭的指令
+//	}
+	static final class AccumCallable implements Callable<Integer> {
+
+		private final int begin;
+		private final int end;
+
+		public AccumCallable(int begin, int end) {
+			super();
+			this.begin = begin;
+			this.end = end;
+		}
+
+		@Override
+		public Integer call() throws Exception {
+			int result = 0;
+			for (int i = begin; i <= end; i++) {
+				result += i;
+				Thread.sleep(100);
+			}
+			System.out.printf("(%s) - 运行结束，结果为 %d\n", Thread.currentThread().getName(), result);
+			return result;
+		}
+	}
+
+}
