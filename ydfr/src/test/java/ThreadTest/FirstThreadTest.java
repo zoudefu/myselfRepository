55181ceba81b71c87924b5038257049d48aad2f2
zdf <you@example.com>
2018-01-08 18:16:48 +0800

    结构提交

--- ydfr/src/test/java/ThreadTest/FirstThreadTest.java
+++ ydfr/src/test/java/ThreadTest/FirstThreadTest.java
@@ -1,4 +1,4 @@
-package ThreadTest;
+package  ThreadTest;
 
 
 public class FirstThreadTest {
--- ydfr/src/test/java/ceshia.java
+++
@@ -1,7 +0,0 @@
-
-public class ceshia {
-public static void main(String[] args){
-	System.out.println("请输入数字一");
-	
-}
-}
