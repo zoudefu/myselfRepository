bfebcaef2ec42a7791435130738533ab786af756
zdf <zdf@sengled.com>
2019-05-27 17:37:32 +0800

    修改配置文件

--- ydfr_front_project/src/router/index.js
+++ ydfr_front_project/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '@/page/login/login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
   ]
 })
